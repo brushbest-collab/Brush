0f5261417e61e3676e29c3ffcdbe282c36987864
brushbest-collab <brushbest@gmail.com>
2025-09-12 22:26:11 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,10 +2,25 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('evi', {
-  getStatus: () => ipcRenderer.invoke('get-status'),
-  downloadModel: () => ipcRenderer.invoke('download-model'),
-  onLog: (cb) => ipcRenderer.on('dl-log', (_e, m) => cb(m)),
-  onStart: (cb) => ipcRenderer.on('dl-start', (_e, d) => cb(d)),
-  onProg: (cb) => ipcRenderer.on('dl-progress', (_e, d) => cb(d)),
-  onDone: (cb) => ipcRenderer.on('dl-done', (_e, d) => cb(d)),
+  // invoke/handle
+  invoke: (channel, data) => ipcRenderer.invoke(channel, data),
+
+  // subscribe progress/log from main
+  on: (channel, listener) => {
+    ipcRenderer.on(channel, (_, payload) => listener(_, payload));
+  },
+  once: (channel, listener) => {
+    ipcRenderer.once(channel, (_, payload) => listener(_, payload));
+  },
+  removeAll: (channel) => ipcRenderer.removeAllListeners(channel),
+
+  // 小工具：帶超時的 invoke
+  invokeWithTimeout: (channel, data, ms = 8000) =>
+    new Promise((resolve, reject) => {
+      const t = setTimeout(() => reject(new Error('timeout')), ms);
+      ipcRenderer
+        .invoke(channel, data)
+        .then((r) => { clearTimeout(t); resolve(r); })
+        .catch((e) => { clearTimeout(t); reject(e); });
+    }),
 });
