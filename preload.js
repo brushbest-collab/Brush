3698a219f8077b2098580e10fe0dae5cabd1fd5f
brushbest-collab <brushbest@gmail.com>
2025-09-12 12:10:52 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,11 @@
+// preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('api', {
-  checkPbs: () => ipcRenderer.invoke('check-pbs')
+contextBridge.exposeInMainWorld('evi', {
+  getStatus: () => ipcRenderer.invoke('get-status'),
+  downloadModel: () => ipcRenderer.invoke('download-model'),
+  onLog: (cb) => ipcRenderer.on('dl-log', (_e, m) => cb(m)),
+  onStart: (cb) => ipcRenderer.on('dl-start', (_e, d) => cb(d)),
+  onProg: (cb) => ipcRenderer.on('dl-progress', (_e, d) => cb(d)),
+  onDone: (cb) => ipcRenderer.on('dl-done', (_e, d) => cb(d)),
 });
