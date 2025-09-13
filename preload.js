febef8339cbe4377d3a969cc22afbed0b36a96e6
brushbest-collab <brushbest@gmail.com>
2025-09-13 11:54:42 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,10 +2,16 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('EVI', {
-  check: () => ipcRenderer.invoke('env-check'),
+  // 環境與模型
+  check:         ()    => ipcRenderer.invoke('env-check'),
   downloadModel: (tag) => ipcRenderer.invoke('download-model', { tag: tag || '' }),
+  onLog:         (fn)  => ipcRenderer.on('log',        (_e, m) => fn(m)),
+  onProgress:    (fn)  => ipcRenderer.on('dl-progress',(_e, p) => fn(p)),
+  onState:       (fn)  => ipcRenderer.on('state',      (_e, s) => fn(s)),
 
-  onLog:       (fn) => ipcRenderer.on('log', (_, m) => fn(m)),
-  onProgress:  (fn) => ipcRenderer.on('dl-progress', (_, p) => fn(p)),
-  onState:     (fn) => ipcRenderer.on('state', (_, s) => fn(s))
+  // 設定
+  getSettings:   ()        => ipcRenderer.invoke('settings:get'),
+  saveSettings:  (data)    => ipcRenderer.invoke('settings:set', data),
+  chooseDir:     ()        => ipcRenderer.invoke('dialog:choose-dir'),
+  openPath:      (target)  => ipcRenderer.invoke('open:path', target)
 });
