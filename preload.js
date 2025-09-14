4d3cb5a94f231785a61103fc9cdb1641f926d97b
brushbest-collab <brushbest@gmail.com>
2025-09-14 12:34:00 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,13 +1,14 @@
 // preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('api', {
-  getState:        () => ipcRenderer.invoke('get-state'),
-  pickModelDir:    () => ipcRenderer.invoke('pick-model-dir'),
-  setModelRoot:    (dir) => ipcRenderer.invoke('set-model-root', dir),
-  downloadModel:   () => ipcRenderer.invoke('download-model'),
-  openDesign:      () => ipcRenderer.invoke('open-design'),
-
-  onLog:      (cb) => ipcRenderer.on('ui-log', (_e, s) => cb(s)),
-  onProgress: (cb) => ipcRenderer.on('ui-progress', (_e, v) => cb(v)),
+contextBridge.exposeInMainWorld('evi', {
+  getState: () => ipcRenderer.invoke('state:get'),
+  pickModelDir: () => ipcRenderer.invoke('dialog:pickModelDir'),
+  startDownload: (opts) => ipcRenderer.invoke('model:download', opts || {}),
+  openDesign: () => ipcRenderer.invoke('app:openDesign'),
+  onLog: (cb) => {
+    const handler = (_e, m) => cb?.(m);
+    ipcRenderer.on('log', handler);
+    return () => ipcRenderer.off('log', handler);
+  },
 });
