00816817c4cdfd2c350518a966589984e211da4d
brushbest-collab <brushbest@gmail.com>
2025-09-15 14:56:11 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('api', {
   getState: () => ipcRenderer.invoke('state:get'),
   pickRoot: () => ipcRenderer.invoke('model:pick-root'),
   startDesign: (state) => ipcRenderer.invoke('design:start', state),
+  downloadModel: (modelRoot) => ipcRenderer.invoke('model:download', modelRoot),
   onLog: (fn) => ipcRenderer.on('log:append', (_e, m) => fn(m))
 });
